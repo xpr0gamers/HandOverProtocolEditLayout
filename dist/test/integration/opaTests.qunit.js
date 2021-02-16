cd69887462f5884dd88d3ada01d3b2bec97bcc32
xpr0gamers <maxi-dietz96@gmx.de>
2021-02-16 11:51:31 +0100

    changes

--- dist/test/integration/opaTests.qunit.js
+++
@@ -1,1 +0,0 @@
-QUnit.config.autostart=false;sap.ui.getCore().attachInit(function(){"use strict";sap.ui.require(["sap/Beutlhauser/HandOverProtocolEditLayout/test/integration/AllJourneys"],function(){QUnit.start()})});
--- dist/test/unit/helper/FakeI18nModel.js
+++
@@ -1,1 +0,0 @@
-sap.ui.define(["sap/ui/model/Model"],function(t){"use strict";return t.extend("sap.Beutlhauser.HandOverProtocolEditLayout.test.unit.helper.FakeI18nModel",{constructor:function(e){t.call(this);this.mTexts=e||{}},getResourceBundle:function(){return{getText:function(t){return this.mTexts[t]}.bind(this)}}})});
